23e7ae47b2b9faaca61519fc56c4b215ac889c64
Felipe Duarte <fcduarte@gmail.com>
2014-06-24 08:22:37 -0700

    Refatoring

--- src/com/fcduarte/showmetweets/activities/SignInWithTwitter.java
+++ src/com/fcduarte/showmetweets/activities/SignInWithTwitter.java
@@ -2,11 +2,8 @@ package com.fcduarte.showmetweets.activities;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
-import twitter4j.conf.Configuration;
-import twitter4j.conf.ConfigurationBuilder;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -17,18 +14,16 @@ import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
 import com.fcduarte.showmetweets.R;
-import com.fcduarte.showmetweets.utils.TwitterPreferencesUtils;
+import com.fcduarte.showmetweets.utils.TwitterUtils;
 
 public class SignInWithTwitter extends Activity {
 
-	protected static final String TWITTER_CONSUMER_KEY = "QZhiS4PAxHk1gfM94K2iSkxpr";
-	protected static final String TWITTER_CONSUMER_SECRET = "aNQg7QPrzNKtZc9RU8Fu85Gj4VFOgGu5qHSYMIK3fWeXSCffFd";
 	protected static final String TWITTER_CALLBACK_URL = "oauth://showmetweets";
 	private static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
 	
 	private ImageButton mSignInTwitterButton;
 	private Twitter mTwitter;
-	private TwitterPreferencesUtils mTwitterPreferencesUtils;
+	private TwitterUtils mTwitterUtils;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +34,10 @@ public class SignInWithTwitter extends Activity {
 		mSignInTwitterButton = (ImageButton) findViewById(R.id.btn_sign_in);
 		mSignInTwitterButton.setOnClickListener(mSignButtonListener);
 		
-		mTwitterPreferencesUtils = new TwitterPreferencesUtils(this);
-		mTwitter = buildTwitter();
+		mTwitterUtils = new TwitterUtils(this);
+		mTwitter = TwitterUtils.getTwitterClient();
 		
-		AccessToken accessToken = mTwitterPreferencesUtils.loadAccessToken();
+		AccessToken accessToken = mTwitterUtils.loadAccessToken();
 		
 		if (accessToken != null) {
 			goToMainScreen(accessToken);
@@ -58,16 +53,6 @@ public class SignInWithTwitter extends Activity {
 		intent.putExtra(HomeActivity.TWITTER_CLIENT_KEY, mTwitter);
 		
 		startActivity(intent);
-	}
-
-	private Twitter buildTwitter() {
-		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.setOAuthConsumerKey(TWITTER_CONSUMER_KEY);
-		builder.setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET);
-		Configuration configuration = builder.build();
-
-		TwitterFactory factory = new TwitterFactory(configuration);
-		return factory.getInstance();
 	}
 
 	private void processTwitterCallback(Uri data) {
@@ -86,12 +71,12 @@ public class SignInWithTwitter extends Activity {
 			@Override
 			public void run() {
 				try {
-					AccessToken accessToken = mTwitter.getOAuthAccessToken(mTwitterPreferencesUtils.loadRequestToken(), verifier);
-					mTwitterPreferencesUtils.saveAccessToken(accessToken.getToken(), accessToken.getTokenSecret());
+					AccessToken accessToken = mTwitter.getOAuthAccessToken(mTwitterUtils.loadRequestToken(), verifier);
+					mTwitterUtils.saveAccessToken(accessToken.getToken(), accessToken.getTokenSecret());
 					goToMainScreen(accessToken);
 				} catch (TwitterException e) {
 					Log.e("SignInWithTwitter", "Error", e);
-					mTwitterPreferencesUtils.clear();
+					mTwitterUtils.clear();
 				}				
 			}
 		});
@@ -107,7 +92,7 @@ public class SignInWithTwitter extends Activity {
 				public void run() {
 					try {
 						RequestToken requestToken = mTwitter.getOAuthRequestToken(TWITTER_CALLBACK_URL);
-						mTwitterPreferencesUtils.saveRequestToken(requestToken.getToken(), requestToken.getTokenSecret());
+						mTwitterUtils.saveRequestToken(requestToken.getToken(), requestToken.getTokenSecret());
 						
 						Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(requestToken.getAuthenticationURL()));
 						intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
@@ -115,7 +100,7 @@ public class SignInWithTwitter extends Activity {
 						SignInWithTwitter.this.startActivity(intent);
 					} catch (TwitterException e) {
 						Log.e("SignIn", "Error requestToken", e);
-						mTwitterPreferencesUtils.clear();
+						mTwitterUtils.clear();
 					}
 				}
 			});
--- src/com/fcduarte/showmetweets/utils/TwitterUtils.java
+++ src/com/fcduarte/showmetweets/utils/TwitterUtils.java
@@ -1,15 +1,21 @@
 package com.fcduarte.showmetweets.utils;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.fcduarte.showmetweets.model.User;
 
-public class TwitterPreferencesUtils {
+public class TwitterUtils {
 	
+	protected static final String TWITTER_CONSUMER_KEY = "QZhiS4PAxHk1gfM94K2iSkxpr";
+	protected static final String TWITTER_CONSUMER_SECRET = "aNQg7QPrzNKtZc9RU8Fu85Gj4VFOgGu5qHSYMIK3fWeXSCffFd";
 	private static final String ACCESS_TOKEN_SECRET_KEY = "access_token_secret";
 	private static final String ACCESS_TOKEN_KEY = "access_token";
 	private static final String REQUEST_TOKEN_SECRET_KEY = "request_token_secret";
@@ -18,7 +24,7 @@ public class TwitterPreferencesUtils {
 	
 	private SharedPreferences prefs;
 	
-	public TwitterPreferencesUtils(Context context) {
+	public TwitterUtils(Context context) {
 		this.prefs = PreferenceManager.getDefaultSharedPreferences(context);
 	}
 	
@@ -42,7 +48,7 @@ public class TwitterPreferencesUtils {
 		SharedPreferences.Editor editor = prefs.edit();
 
 		editor.putString(ACCESS_TOKEN_KEY, accessToken);
-		editor.putString(ACCESS_TOKEN_SECRET_KEY, accessToken);
+		editor.putString(ACCESS_TOKEN_SECRET_KEY, accessTokenSecret);
 		
 		editor.commit();
 	}
@@ -78,6 +84,16 @@ public class TwitterPreferencesUtils {
 	public boolean userChanged(User user) {
 		String loggedUser = loadLoggedUser();
 		return loggedUser == null || !user.getUsername().equals(loggedUser);
+	}
+	
+	public static Twitter getTwitterClient() {
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey(TWITTER_CONSUMER_KEY);
+		builder.setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET);
+		Configuration configuration = builder.build();
+
+		TwitterFactory factory = new TwitterFactory(configuration);
+		return factory.getInstance();
 	}
 
 }
